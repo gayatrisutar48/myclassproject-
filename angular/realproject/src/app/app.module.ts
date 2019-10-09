bee4434285673b39e373eb522453a924f07ba20a
gayatrisutar48 <g.gsutar48@gmail.com>
2019-10-09 16:20:24 +0530

    header(navbar) : top-bar, main-bar

--- angular/realproject/src/app/app.module.ts
+++ angular/realproject/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TopBarComponent } from './header/top-bar/top-bar.component';
 import { MainBarComponent } from './header/main-bar/main-bar.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     TopBarComponent,
-    MainBarComponent
+    MainBarComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule
